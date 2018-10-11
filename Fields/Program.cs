336b0d4398ada7c3baf6649ed18288f5d5d5a141
yeasin habib <yeasinhabib1994@gmail.com>
2018-10-11 16:52:18 +0600

    Add project files.

---
+++ Fields/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            customer.Promote();
+
+            Console.WriteLine(customer.Orders.Count);
+        }
+    }
+
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders = new List<Order>();
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            : this(id) //calls the previous constructor to get the id.
+        {
+            this.Name = name;
+        }
+
+        public void Promote()
+        {
+            Orders = new List<Order>(); // Orders list will be re-initialized and will have 0 orders.
+        }
+    }
+    public class Order
+    {
+
+    }
+}
